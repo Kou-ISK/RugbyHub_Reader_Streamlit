7ff185acbf5ad6398326e1c5b243a3f00082498b
k-isaka <isk.rugby@gmail.com>
2023-02-23 02:09:24 +0900

    feat: add if statement

--- RugbyHub_Reader_Streamlit/app.py
+++ RugbyHub_Reader_Streamlit/app.py
@@ -18,11 +18,12 @@ if uploaded_file is not None:
     if action is not None:
         act = df.loc[df['action'] == action]
         st.write("選択したactionに紐づくx_coordとy_coordをプロットしています。順次機能追加予定です。")
-        read_xml.plot_by_action(df, action, team_id)
-        act
-        with action == 'Ruck':
+        if action == 'Ruck':
             read_xml.ruck_speed(df, team_id)
-        with action == 'Kick':
+        elif action == 'Kick':
             read_xml.kick(df, team_id)
-        with action == 'Lineout Throw':
+        elif action == 'Lineout Throw':
             read_xml.lineout(df, team_id)
+        else:
+            read_xml.plot_by_action(df, action, team_id)
+            act
