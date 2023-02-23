38eedceffacd9b8521416c1f2ae14e3d472b52e1
k-isaka <isk.rugby@gmail.com>
2023-02-23 12:40:44 +0900

    feat: separated plotting method from read_xml module 2

--- RugbyHub_Reader_Streamlit/app.py
+++ RugbyHub_Reader_Streamlit/app.py
@@ -1,7 +1,7 @@
 import read_xml as read_xml
 import streamlit as st
 import pandas as pd
-from plot import Plot_method
+import plot
 
 st.title("RugbyHub Reader")
 # st.checkbox("チェックボックス")
@@ -17,7 +17,7 @@ if uploaded_file is not None:
     df
     # データフレームのカラムを選択肢にする。複数選択
     item = st.multiselect("可視化するカラム", df.columns)
-    p = Plot_method(df, team_id)
+    p = plot.Plot_method(df, team_id)
     if item is not None:
         df[item]
     if action is not None:
